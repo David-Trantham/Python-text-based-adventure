6417915fa786abb7854a1b9db31cb4ff306636b0
Green <greeniscool1234@gmail.com>
2024-02-02 17:15:22 -0500

    Implement room list and room finder method

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-player_name = input("Please enter your name: ")
+# An example mapping of the rooms, represented as a 2d list:
+current_room = 1
+room_map = [[15, 16, 17, 18, 19, 20],
+            [14, 0, 0, 0, 0, 0],
+            [13, 12, 11, 10, 0, 0],
+            [0, 9, 8, 7, 6, 0],
+            [0, 0, 3, 4, 5, 0],
+            [0, 0, 2, 1, 0, 0]]
 
-default_directions = ["Left", "Right", "Forward", "Back"]
-rooms = {0: default_directions}
+#This function returns the index of the specified room within the room map.
+def find_room(room_id):
+    for i, x in enumerate(room_map):
+        if room_id in x:
+            return (i,x.index(room_id))
 
-room_number = 0
-player_input = ""
+def move(direction):
+    try:
+        room_location = find_room(current_room)
+        print(room_location(1))
+    except:
+        print("wow")
 
-if room_number == 0:
-    print("Welcome to the game!")
-    player_input = input("Do you want to go left or right? (L/R)")
+def main():
+    print(find_room(1))
+    print(room_map[5][3])
+    player_name = input("Please enter your name: ")
 
-if player_input.upper() == "R":
-    room_number = 1
-elif player_input.upper == "L":
-    room_number = 2
+    default_directions = ["Left", "Right", "Forward", "Back"]
+    rooms = {0: default_directions}
+
+    room_number = 0
+    player_input = ""
+
+#After initializing global variables, start the program at the main function
+if __name__ == '__main__':
+    main()
