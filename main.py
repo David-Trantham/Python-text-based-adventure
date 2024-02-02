3d5412d7b39e8b709906b7b00ef118cb7288525a
Green <greeniscool1234@gmail.com>
2024-02-02 17:45:11 -0500

    Add check_room_at_location lookup feature

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # An example mapping of the rooms, represented as a 2d list:
-current_room = 1
 room_map = [[15, 16, 17, 18, 19, 20],
             [14, 0, 0, 0, 0, 0],
             [13, 12, 11, 10, 0, 0],
             [0, 9, 8, 7, 6, 0],
             [0, 0, 3, 4, 5, 0],
             [0, 0, 2, 1, 0, 0]]
+current_room = 1
+current_room_location = (5,3)
 
 #This function returns the index of the specified room within the room map.
 def find_room(room_id):
@@ -13,16 +14,29 @@ def find_room(room_id):
         if room_id in x:
             return (i,x.index(room_id))
 
-def move(direction):
+def check_room_at_location(room_location):
+    return room_map[room_location[0]][room_location[1]]
+
+def move(
     try:
-        room_location = find_room(current_room)
-        print(room_location(1))
+        match direction:
+            case "Forward":
+            case "Back":
+            case "Left":
+            case "Right":
+            case default:
+        if newroom == 0: Exception
+        print(room_location[1])
     except:
-        print("wow")
+        print("Nope, can't move that way!")
 
 def main():
     print(find_room(1))
     print(room_map[5][3])
+    print(check_room_at_location(current_room_location))
+    # Should move us to room 4
+    move("")
+    print(current_room)
     player_name = input("Please enter your name: ")
 
     default_directions = ["Left", "Right", "Forward", "Back"]
